66e173d58275bee95fe26fc63c1702d9f81a0543
Minstrel157 <mamonov_nikita2006@mail.ru>
2024-11-14 00:46:04 +0300

    added new models

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import MainPage from "./pages/MainPage/mainPage"
 
 function App() {
-  const [image, setImage] = useState([])
+  // const [image, setImage] = useState([])
 
-  useEffect(() => {
-    const fetchImage = async() =>
-    {
-      try{
-        const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=kYE5SvZaA2zbkTrNfpDfeLXLAD3yXXaDyZlIdTVB")
-        setImage(response.data)
-      }
-      catch(e){
-        console.log(e)
-      }
-    }
-    fetchImage()
-  },[])
+  // useEffect(() => {
+  //   const fetchImage = async() =>
+  //   {
+  //     try{
+  //       const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=kYE5SvZaA2zbkTrNfpDfeLXLAD3yXXaDyZlIdTVB")
+  //       setImage(response.data)
+  //     }
+  //     catch(e){
+  //       console.log(e)
+  //     }
+  //   }
+  //   fetchImage()
+  // },[])
 
-  console.log(image)
+  // console.log(image)
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
